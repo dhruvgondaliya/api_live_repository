4eac4bf54364001123842b2cac6efcc7abb5dba2
dhruv@gmail.com <dhruv@gmail.com>
2024-09-19 17:17:00 +0530

    product new code

--- database/table.sql
+++ database/table.sql
@@ -26,18 +26,11 @@ create table add_products(
     product_price int,
     product_brand VARCHAR(100) NOT NULL,
     product_colour VARCHAR(100) NOT NULL,
+    product_discount VARCHAR(100) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    return_policy VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    foreign key (user_id) references users(id)
-)
-
-create table carts(
-    id int AUTO_INCREMENT PRIMARY KEY,
-    details_id int,
-    product_name VARCHAR(100) NOT NULL,
-    product_price int,
-    product_discount VARCHAR(100) NOT NULL,
-    quantity int,
     foreign key (user_id) references users(id)
 )
 
@@ -52,5 +45,51 @@ create table details(
     product_colour VARCHAR(100) NOT NULL,
     category VARCHAR(100) NOT NULL,
     return_policy VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     foreign key (product_id) references add_products(id) 
 )
+
+create table carts(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    details_id int,
+    product_name VARCHAR(100) NOT NULL,
+    product_price int,
+    product_discount VARCHAR(100) NOT NULL,
+    quantity int,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    foreign key (user_id) references users(id)
+)
+
+create table home_screens(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    product_id int,
+    product_name VARCHAR(100) NOT NULL,
+    product_discription VARCHAR(100) NOT NULL,
+    product_price int,
+    product_discount VARCHAR(5) NOT NULL,
+    product_brand VARCHAR(100) NOT NULL,
+    product_colour VARCHAR(100) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    return_policy VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    foreign key (product_id) references add_products(id)
+)
+
+create table delivery_details(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    cart_id int,
+    your_name VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    city VARCHAR(20)NOT NULL,
+    pincode int NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    phone_number VARCHAR(100) NOT NULL,
+    product_price int,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    foreign key (cart_id) references carts(id);
+)
+ 
